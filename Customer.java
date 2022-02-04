8f857c9f592a9cab737fd395a70f70e75c5b0219
Lucas Santana <lucasgabrielelt@gmail.com>
2022-02-04 20:07:53 -0300

    Commit 2

--- Customer.java
+++ Customer.java
@@ -16,6 +16,26 @@ public class Customer {
    public String getName (){
       return _name;
    }
+
+   public double amountFor(Rental each){ //determine amounts for each line
+      double thisAmount = 0;
+      switch (each.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+          thisAmount += 2;
+          if (each.getDaysRented() > 2)
+              thisAmount += (each.getDaysRented() - 2) * 1.5;
+          break;
+        case Movie.NEW_RELEASE:
+          thisAmount += each.getDaysRented() * 3;
+          break;
+        case Movie.CHILDRENS:
+          thisAmount += 1.5;
+          if (each.getDaysRented() > 3)
+              thisAmount += (each.getDaysRented() - 3) * 1.5;
+            break;
+      }
+      return thisAmount;
+   }
   
   public String statement() {
      double totalAmount = 0;
@@ -25,24 +45,7 @@ public class Customer {
      while (rentals.hasMoreElements()) {
         double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
-
-        //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
-
+        thisAmount = amountFor(each);
         // add frequent renter points
         frequentRenterPoints ++;
         // add bonus for a two day new release rental
