dd14275167d5a90c794819e0052a385215e12d2c
Lucas Santana <lucasgabrielelt@gmail.com>
2022-02-04 21:16:39 -0300

    Commit 10

--- Movie.java
+++ Movie.java
@@ -27,20 +27,29 @@ public class Movie {
   public double getCharge(int daysRented){
     double thisAmount = 0;
     switch (getPriceCode()) {
-      case Movie.REGULAR:
+      case REGULAR:
         thisAmount += 2;
         if (daysRented > 2)
             thisAmount += (daysRented - 2) * 1.5;
         break;
-      case Movie.NEW_RELEASE:
+      case NEW_RELEASE:
         thisAmount += daysRented * 3;
         break;
-      case Movie.CHILDRENS:
+      case CHILDRENS:
         thisAmount += 1.5;
         if (daysRented > 3)
             thisAmount += (daysRented - 3) * 1.5;
           break;
     }
     return thisAmount;
+  }
+
+  public int getFrequentRenterPoints(int daysRented){
+    int frequentRenterPoints=0;
+    frequentRenterPoints ++;
+        // add bonus for a two day new release rental
+        if ((getPriceCode() == NEW_RELEASE) &&
+            daysRented > 1) frequentRenterPoints ++;
+    return frequentRenterPoints;
   }
 }
