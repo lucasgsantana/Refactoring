d7c255082fb2866d44aba0e439ccc3d09eed2895
Lucas Santana <lucasgabrielelt@gmail.com>
2022-02-04 23:28:48 -0300

    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,8 +1,5 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
-  public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
+  public String[] Content(Customer aCustomer) {
       String content[] = 
       {
         "<H1>Rentals for <EM>",
@@ -11,20 +8,6 @@ public class HtmlStatement extends Statement {
         "On this rental you earned <EM>",
         "</EM> frequent renter points<P>"
       };
-      String result = content[0] + aCustomer.getName() +
-      content[1];
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for each rental
-         result += each.getMovie().getTitle()+ content[2] +
-         String.valueOf(each.getCharge()) + content[3];
-      }
-      //add footer lines
-      result += content[4] +
-      String.valueOf(aCustomer.getTotalCharge()) + content[5];
-      result += content[6] + 
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      content[7];
-      return result;
+      return content;
    }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,8 +1,7 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
-  public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
+  public String[] Content(Customer aCustomer) {
       String content[] = 
       {
         "Rental Record for ",
@@ -11,20 +10,6 @@ public class TextStatement extends Statement {
         "\n You earned ",
         " frequent renter points"
       };
-      String result = content[0] + aCustomer.getName() +
-      content[1];
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for each rental
-         result += each.getMovie().getTitle()+ content[2] +
-         String.valueOf(each.getCharge()) + content[3];
-      }
-      //add footer lines
-      result += content[4] +
-      String.valueOf(aCustomer.getTotalCharge()) + content[5];
-      result += content[6] + 
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      content[7];
-      return result;
+      return content;
    }
 }
